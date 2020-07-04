57ed7e293739a0ed59f6ad8551ccd15aee3fc580
Dubnytskyi <dubnytskyi.coding@gmail.com>
2020-07-04 20:41:10 +0200

    'working_on_basic_info_section'

--- .history/src/Components/PopUpComponents/Abilities_20200704201549.js
+++ .history/src/Components/PopUpComponents/Abilities_20200704201549.js
@@ -12,7 +12,7 @@ const Abilities = props => {
                 </div>
                 {props.pokemon.abilities.map((item, i) => {
                     return (
-                        <div className={'abilities-grid-container abilities-column-container'} key={i}>
+                        <div className={'abilities-grid-container'} key={i}>
                             <div className={'table-column'} >{item.ability.name.charAt(0).toUpperCase() + item.ability.name.slice(1)}</div>
                             <div className={'table-column'} >{item.slot}</div>
                             <div className={'table-column'} >{item.is_hidden ? 'Yes' : 'No'}</div>
